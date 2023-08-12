1971f25f094c8cc67ac4e97faa236c15e4ce1185
YoungHan Jo <verin4494@gmail.com>
2023-08-12 15:06:25 +0900

    commit

--- ch7/prac2/prac2.go
+++ ch7/prac2/prac2.go
@@ -6,11 +6,9 @@ func F(n int) int {
 	if n < 2 {
 		return n
 	}
-
 	return F(n-2) + F(n-1)
 }
 
 func main() {
 	fmt.Println(F(9))
-
 }
---
+++ ch8/ex8.1/ex8.1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	const C int = 10
+
+	var a int = 20
+
+	fmt.Println(&a)
+	// fmt.Println(&C)
+}
